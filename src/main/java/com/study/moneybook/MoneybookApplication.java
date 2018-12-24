dbc18729a4df26de67e34570cd96743981d1e0fe
circleGiven <circlegiven@gmail.com>
2018-12-24 16:08:16 +0900

    init project

---
+++ src/main/java/com/study/moneybook/MoneybookApplication.java
@@ -0,0 +1,14 @@
+package com.study.moneybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneybookApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/study/moneybook/MoneybookApplicationTests.java
@@ -0,0 +1,17 @@
+package com.study.moneybook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneybookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
