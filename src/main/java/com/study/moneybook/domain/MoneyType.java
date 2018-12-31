2aacf13a2b8a0c2c64da6628680883a3f41a4b1e
circleGiven <circlegiven@gmail.com>
2018-12-31 17:01:48 +0900

    #1 create moneybook domain
    - 가계부 도메인
    - 가계부 타입
    - 비용
    - 결제수단

---
+++ src/main/java/com/study/moneybook/domain/MoneyType.java
@@ -0,0 +1,17 @@
+package com.study.moneybook.domain;
+
+public enum  MoneyType {
+    SALARY,                 // 월급
+    REIMBURSEMENT,          // 환급
+    ALLOWANCE,              // 용돈
+    FOOD_EXPENSE,           // 식비
+    TRANSPORT_EXPENSE,      // 교통비
+    COMMUTE_EXPENSE,        // 통신비
+    LIVING_EXPENSE,         // 생활비
+    UTILITY_CHARGE,         // 공과금
+    CULTURAL_EXPENSE,       // 문화비
+    MEDICAL_EXPENSE,        // 의료비
+    INSURANCE_EXPENSE,      // 보험비
+    SAVING,                 // 저축
+    ETC                     // 기타
+}
---
+++ src/main/java/com/study/moneybook/domain/WorkType.java
@@ -0,0 +1,6 @@
+package com.study.moneybook.domain;
+
+public enum WorkType {
+    PROFIT, // 수입
+    EXPENSE // 지출
+}
