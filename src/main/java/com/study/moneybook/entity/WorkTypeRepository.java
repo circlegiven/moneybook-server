d68051e6024b35a87c1eac78046585b3704eeb5e
circleGiven <circlegiven@gmail.com>
2019-01-01 21:47:39 +0900

    #2 create moneybook repository
    - repository 생성

---
+++ src/main/java/com/study/moneybook/entity/WorkTypeRepository.java
@@ -0,0 +1,8 @@
+package com.study.moneybook.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkTypeRepository  extends JpaRepository {
+}
