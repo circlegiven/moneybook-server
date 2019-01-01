7e8732819e3383ecdb1d7cd8bf0dbb62f202e02b
circleGiven <circlegiven@gmail.com>
2019-01-01 18:22:22 +0900

    #2 create moneybook entity
    - entity 생성

---
+++ src/main/java/com/study/moneybook/entity/WorkEntity.java
@@ -0,0 +1,37 @@
+package com.study.moneybook.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "work")
+public class WorkEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(targetEntity = WorkTypeEntity.class)
+    private WorkTypeEntity type;
+
+    @ManyToOne(targetEntity = PaymentTypeEntity.class)
+    private PaymentTypeEntity paymentType;
+
+    @ManyToOne(targetEntity = MoneyTypeEntity.class)
+    private MoneyTypeEntity moneyType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date targetTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedTime;
+}
---
+++ src/main/java/com/study/moneybook/entity/WorkTypeEntity.java
@@ -0,0 +1,18 @@
+package com.study.moneybook.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "work_type")
+public class WorkTypeEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
